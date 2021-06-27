bc4fa1815079208ad8cbaff21770632fd44af9d5
cyang <1018225817@qq.com>
2021-06-27 11:43:40 +0800

    add reverse

---
+++ Reverse/Reverse/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Activities;
+using System.Activities.Statements;
+using System.Linq;
+
+namespace Reverse
+{
+    class Solution
+    {
+        public static int Reverse(int x)
+        {
+            long res = 0;
+
+            while (x != 0)
+            {
+                res = (x % 10) + res * 10;
+                x /= 10;
+            }
+
+            if (res > Int32.MaxValue || res < Int32.MinValue)
+                res = 0;
+
+            return (int)res;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 321;            
+            
+            int res = Reverse(a);
+            Console.WriteLine(res);
+        }
+    }
+}
