b5e459132bd2a4d48f7647f366df13f94d0ccfd2
cyang <1018225817@qq.com>
2021-06-27 11:05:02 +0800

    add HashTable resolution

--- TwoSum/Solution.cs
+++ TwoSum/Solution.cs
@@ -4,7 +4,25 @@ namespace TwoSum
 {
     public class Solution
     {
-        public int[] TwoSum(int[] nums, int target)
+        //暴力法
+        //public int[] TwoSum(int[] nums, int target)
+        //{
+        //    for (int i = 0; i < nums.Length - 1; i++)
+        //    {
+        //        for (int j = i + 1; j < nums.Length; j++)
+        //        {
+        //            if (nums[i] + nums[j] == target)
+        //            {
+        //                int[] res = new int[] { i, j };
+        //                return res;
+        //            }
+        //        }
+        //    }
+        //    return new int[] {0, 0};
+        //}
+
+        //哈希表
+        public static int[] TwoSum(int[] nums, int target)
         {
             var dict = new Dictionary<int, int>();
 
@@ -14,11 +32,20 @@ namespace TwoSum
 
                 if (dict.TryGetValue(otherNumber, out var index))
                     return new int[2] { index, i };
-            
+
                 dict.Add(nums[i], i);
             }
 
             return new int[2];
+        }
+
+        public static void Main()
+        {
+            int[] nums = new int[] { 2, 7, 11, 15 };
+            int target = 18;
+
+            int[] res = TwoSum(nums, target);
+            System.Console.WriteLine("{0}, {1}", res[0], res[1]);
         }
     }
 }
